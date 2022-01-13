ab8fc9535a9549a0791881cfa2d657993f322780
tscotce <tatyana.celovsky@gmail.com>
2022-01-13 17:20:58 -0500

    Set up basics of AddItem

---
+++ src/components/AddItem.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { db } from "./lib/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
+
+const AddItem = () => {
+  return <div className="add-item"></div>;
+};
+
+export default AddItem;
